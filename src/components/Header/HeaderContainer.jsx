478e133fea771a46b6ba3b277d7ece623039b419
ilich <ilich@templatemonster.me>
2021-06-03 13:56:02 +0300

    Thunks are was entered into the HeaderContainer

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 import Header from './Header';
 import { setAuthUserData, authUser } from '../../redux/auth-reducer '
 import { connect } from 'react-redux';
-import { authAPI } from '../../api/api'
+
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
         this.props.authUser()
-        // authAPI.getAuth().then(data => {
-        //         if (data.resultCode === 0) {
-        //             let {id, email, login} = data.data
-        //             this.props.setAuthUserData(id, email, login)
-        //         }
-        //     })
     }
 
     render() {
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,9 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import profileReducer from './profile-reducer';
-import dialogsReducer from './dialogs-reducer';
-import sidebarReducer from './sidebar-reducer';
-import UsersReducer from "./users-reducer";
-import AuthReducer from "./auth-reducer ";
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import profileReducer from './profile-reducer'
+import dialogsReducer from './dialogs-reducer'
+import sidebarReducer from './sidebar-reducer'
+import UsersReducer from "./users-reducer"
+import AuthReducer from "./auth-reducer "
 import thunkMiddleware from 'redux-thunk'
 
 let reducers = combineReducers({
